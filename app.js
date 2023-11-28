3c0e2d7f8b57accaa4e3f0428e673b6742c1e511
Sihanip <hanifkadot@gmail.com>
2023-11-29 00:18:44 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+// router
+const categoriesRouter = require('./app/api/v1/categories/routes');
+const imagesRouter = require('./app/api/v1/images/routes');
+const talentsRouter = require('./app/api/v1/talents/routes');
+const eventsRouter = require('./app/api/v1/events/routes');
+
+const v1 = '/api/v1/cms';
+
+// middlewares //harus dibawah router
+const notFoundMiddleware = require('./app/middlewares/not-found');
+const handleErrorMiddleware = require('./app/middlewares/handler-error');
+// middlewares
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome Api Semina'
+    })
+});
+
+app.use(v1, categoriesRouter);
+app.use(v1, imagesRouter);
+app.use(v1, talentsRouter);
+app.use(v1, eventsRouter);
+
+// penggunaan middlewares dibawah router
+app.use(notFoundMiddleware);
+app.use(handleErrorMiddleware);
+
+
+module.exports = app;
